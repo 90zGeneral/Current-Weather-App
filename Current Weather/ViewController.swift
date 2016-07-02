69c7efd6561562375c02d70fb693b5cee790b73d
Roydon Jeffrey <brushdam14823@gmail.com>
2016-07-02 10:45:23 -0400

    All functionalities of the app is working but there are a few minor bugs to be fixed

--- Current Weather/ViewController.swift
+++ Current Weather/ViewController.swift
@@ -16,43 +16,78 @@ class ViewController: UIViewController {
     
     @IBAction func findWeather(sender: AnyObject) {
         
+        var successfullyFound = false
         
+        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/\(cityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-"))/forecasts/latest")
+        
+        if let url = attemptedUrl {
+        
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, err) in
+            
+                if let urlContent = data {
+                
+                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                    let webContentArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                
+                    if webContentArray.count > 1 {
+                    
+                        let weatherSummaryArray = webContentArray[1].componentsSeparatedByString("</span></span></span></p><div")
+                    
+                        if weatherSummaryArray.count > 1 {
+                            
+                            successfullyFound = true
+                        
+                            let weatherSummary = weatherSummaryArray[0]
+                        
+                            let result = weatherSummary.stringByReplacingOccurrencesOfString("&deg;", withString: "°")
+                        
+                            //Whenever a UI item is being affected, wrap it inside a dispatch closure to end the queue quickly and update UI item.
+                            dispatch_async(dispatch_get_main_queue(), {
+                                
+                                self.weatherSummaryLabel.text = result
+                                self.cityTextField.text = ""
+                                
+                            })
+        
+                        }
+                    
+                    }
+                
+                }
+            
+            }
+            
+            task.resume()
+            
+        }else {
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                self.weatherSummaryLabel.text = "That's NOT a city name. Try again!"
+                self.cityTextField.text = ""
+                
+            })
+            
+        }
+        
+        if self.cityTextField.text == "" {
+            
+            weatherSummaryLabel.text = "Please Enter a City Name"
+            
+        }else if successfullyFound == false {
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                self.weatherSummaryLabel.text = "City name NOT found. Maybe check spelling and try again."
+            })
+            
+        }
         
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let url = NSURL(string: "http://www.weather-forecast.com/locations/Brooklyn/forecasts/latest")!
-        
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, err) in
-            
-            if let urlContent = data {
-                
-                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
-                
-                let webContentArray = webContent?.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
-                
-                if webContentArray!.count > 0 {
-                    
-                    let weatherSummaryArray = webContentArray![1].componentsSeparatedByString("</span></span></span></p><div")
-                    
-                    if weatherSummaryArray.count > 0 {
-                        
-                        let weatherSummary = String(weatherSummaryArray[0])
-                        
-                        let result = weatherSummary.stringByReplacingOccurrencesOfString("&deg;", withString: "°")
-                        
-                        print(result)
-                    }
-                    
-                }
-                
-            }
-            
-        }
-        task.resume()
         
     }
 
